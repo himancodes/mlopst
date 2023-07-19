23a48ee13fc4289bce471e3d56e9132ad97819f3
himancodes <133040477+himancodes@users.noreply.github.com>
2023-07-19 10:45:28 +0530

    deploy_dtmodel.py

--- deploy_dtmodel.py
+++ deploy_dtmodel.py
@@ -11,9 +11,9 @@ st.header("Diamond Price Model")
 st.sidebar.header("This is a web app")
 
 carat = float(st.number_input("Enter carat"))
-cut = st.sidebar.slider("Select cut to get yhat", 0, 1, 2, 3, 4)
-color = st.sidebar.slider("Select color to get yhat", 0, 1, 2, 3, 4,5,6)
-clarity = st.sidebar.slider("Select clarity to get yhat", 0, 1, 2, 3, 4,5,6,7,8)
+cut = st.sidebar.slider("Select cut to get yhat", 0, 4)
+color = st.sidebar.slider("Select color to get yhat", 0, 6)
+clarity = st.sidebar.slider("Select clarity to get yhat", 0, 7)
 
 
 
